e2f2f10f414da3140133250db3ccc520386175ca
Victor_Morales_Perez <vmorales@atsistemas.com>
2022-04-13 18:47:18 +0200

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,8 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
+//Prime Ng
+import { ButtonModule } from 'primeng/button';
 
 @NgModule({
   declarations: [
